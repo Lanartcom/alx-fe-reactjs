739547f2a1aebdc6f8506d3cef087081e0d28b36
Lanartcom <lanartcom@outlook.com>
2024-11-03 19:23:18 +0100

    created a user profile card using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,12 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import WelcomeMessage from './components/WelcomeMessage';
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import WelcomeMessage from './components/WelcomeMessage'
+import UserProfile from './components/UserProfile'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,6 +18,8 @@ function App() {
       <main>
         <MainContent />
         <WelcomeMessage />
+        
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </main>
 
       <div>
