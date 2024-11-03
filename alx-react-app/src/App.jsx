035014a0c5e8963d07090cd6f2ebecd9cdb14205
Lanartcom <lanartcom@outlook.com>
2024-11-03 19:14:15 +0100

    added specific components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,22 +1,29 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-         <div>
-            <WelcomeMessage />
-        </div>
+    <div>
+      <Header />
+
+      <main>
+        <MainContent />
+        <WelcomeMessage />
+      </main>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -32,8 +39,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
