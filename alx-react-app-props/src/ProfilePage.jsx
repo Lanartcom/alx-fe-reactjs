981acc3653b6a5640bcd53cc415c91a3b71c48ce
Lanartcom <lanartcom@outlook.com>
2024-11-04 21:36:49 +0100

    practiced refactoring task

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return <UserInfo />; // No need to pass userData anymore
+}
+
+export default ProfilePage;
