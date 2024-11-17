549faa508d80f3030bb17f4aa2195e53aede71de
Lanartcom <lanartcom@outlook.com>
2024-11-17 19:33:54 +0100

    fixed routing in app.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
 import RecipeDetails from './components/RecipeDetails'
@@ -8,27 +8,29 @@ import DeleteRecipeButton from './components/DeleteRecipeButton'
 
 const App = () => {
   return (
-    <div style={{ padding: '2rem' }}>
-      <h1>Recipe Sharing Application</h1>
+    <Router>
+      <div style={{ padding: '2rem' }}>
+        <h1>Recipe Sharing Application</h1>
 
-      {/* Define Routes for different views */}
-      <Routes>
-        {/* Home route displaying all recipes */}
-        <Route path="/" element={<RecipeList />} />
+        {/* Define Routes for different views */}
+        <Routes>
+          {/* Home route displaying all recipes */}
+          <Route path="/" element={<RecipeList />} />
 
-        {/* Route to add a new recipe */}
-        <Route path="/add-recipe" element={<AddRecipeForm />} />
+          {/* Route to add a new recipe */}
+          <Route path="/add-recipe" element={<AddRecipeForm />} />
 
-        {/* Route to view individual recipe details */}
-        <Route path="/recipe/:id" element={<RecipeDetails />} />
+          {/* Route to view individual recipe details */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
 
-        {/* Route to edit an existing recipe */}
-        <Route path="/recipe/:id/edit" element={<EditRecipeForm />} />
+          {/* Route to edit an existing recipe */}
+          <Route path="/recipe/:id/edit" element={<EditRecipeForm />} />
 
-        {/* Route to delete a recipe */}
-        <Route path="/recipe/:id/delete" element={<DeleteRecipeButton />} />
-      </Routes>
-    </div>
+          {/* Route to delete a recipe */}
+          <Route path="/recipe/:id/delete" element={<DeleteRecipeButton />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,20 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css' // Ensure your styles are included
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
-
-// Define routes
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-])
 
 // Render the application
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 )
