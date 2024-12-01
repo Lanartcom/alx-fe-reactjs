b8ab637b368efbd49a183d1618610ab6e41630cf
Lanartcom <lanartcom@outlook.com>
2024-12-01 16:03:10 +0100

    Styling a Provided React Component with Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,7 +1,11 @@
-export default function App() {
+import UserProfile from './components/UserProfile';
+
+function App() {
   return (
-    <div className="h-screen flex justify-center items-center bg-blue-500 text-white text-3xl">
-      Tailwind CSS is Successfully Integrated!
+    <div>
+      <UserProfile />
     </div>
   );
 }
+
+export default App;
