34a7b728cbacc3bf4f011193d1bd2b98cc3e8c91
Lanartcom <lanartcom@outlook.com>
2024-12-01 15:30:21 +0100

    setting up tailwind css in a new react project

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-blue-500 text-white text-3xl">
+      Tailwind CSS is Successfully Integrated!
+    </div>
+  );
+}
