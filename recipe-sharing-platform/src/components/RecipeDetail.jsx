72abbc7840fbc78138a8ad33bf9dfdabb566814b
Lanartcom <lanartcom@outlook.com>
2024-12-08 20:54:18 +0100

    added instructions to recipedetails

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -58,11 +58,11 @@ const RecipeDetail = () => {
               <li key={index}>{ingredient}</li>
             ))}
           </ul>
-          <h2 className="text-2xl font-semibold mb-2">Cooking Instructions</h2>
+          <h2 className="text-2xl font-semibold mb-2">Instructions</h2>
           <ol className="list-decimal list-inside text-gray-700">
-            {recipe.steps.map((step, index) => (
+            {recipe.instructions.map((instruction, index) => (
               <li key={index} className="mb-2">
-                {step}
+                {instruction}
               </li>
             ))}
           </ol>
