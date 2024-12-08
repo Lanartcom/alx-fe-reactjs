7aaab9741dc801aa309dda335abb929226623092
Lanartcom <lanartcom@outlook.com>
2024-12-08 19:20:33 +0100

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-blue-500 text-center text-xl">
+      Tailwind CSS is working!
+    </div>
+  );
+}
+
+export default App;
